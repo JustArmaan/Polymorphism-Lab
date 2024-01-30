30b5be58458fb214c478a29f14983895be83844a
JustArmaan <singharmaanbrar@gmail.com>
2024-01-30 12:28:27 -0800

    create teste.ts, finish linkedlistgroups

--- app.ts
+++ app.ts
@@ -16,11 +16,13 @@ export class SortUtil {
     let lastUnsorted = length - 1;
     while (!isSorted) {
       isSorted = true;
-      for (let i = 0; i < lastUnsorted; i++) {
-        if (this.collection.compare(i, i + 1)) {
-          this.collection.swap(i, i + 1);
-          isSorted = false;
-        }
+      for (let j = 0; j<lastUnsorted; j++) {
+          for (let i = 0; i < lastUnsorted; i++) {
+            if (this.collection.compare(i, i + 1)) {
+              this.collection.swap(i, i + 1);
+              isSorted = false;
+            }
+      }
       }
       lastUnsorted--;
     }
---
+++ test.ts
@@ -0,0 +1,25 @@
+import { CharecterGroup } from "./CharacterGroup";
+import { LinkedListGroup } from "./LinkedListGroup";
+import { NumberGroup } from "./NumberGroup";
+import { SortUtil } from "./app";
+
+const linkedList1 = new LinkedListGroup();
+linkedList1.add(4);
+linkedList1.add(2);
+linkedList1.add(3);
+linkedList1.add(1);
+
+const numberList1 = new NumberGroup([4, 2, 3, 1]);
+const string1 = new CharecterGroup("xyzabc");
+
+const test1 = new SortUtil(linkedList1);
+test1.sort();
+console.log(test1.collection)
+
+const test2 = new SortUtil(numberList1);
+test2.sort();
+console.log(test2.collection)
+
+const test3 = new SortUtil(string1);
+test3.sort();
+console.log(test3.collection)
